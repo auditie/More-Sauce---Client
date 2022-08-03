6f98775837d1e7ef7fb8b40866217cd71409b490
Audun <audunyoung@gmail.com>
2022-08-02 19:48:07 -0600

    Work on inital structure of login page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import PageHeader from './components/PageHeader/PageHeader';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <PageHeader />
       <Switch>
         <Route path='/' exact component={HomePage} />
-        {/* <Route path='/login' component={LoginPage} />*/}
+        <Route path='/login' component={LoginPage} />
         {/* <Route path='/signup' component={SignupPage} />*/}
         {/* <Route path='/login' component={LoginPage} />*/}
         {/* <Route path='/restaurants' component={RestaurantList} />*/}
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -10,13 +10,13 @@ class LoginPage extends Component {
         return (
             <div className='login-page'>
                 <form className='login-page__user-info'>
-                    <label for='email'>Email</label>
-                    <input type='text' id='email' name='email' className='login-page__user-info-input'></input>
-                    <label for='password'>Password</label>
+                    <label for='email' className='login-page__user-info-label'>Email</label>
+                    <input type='text' id='email' name='email' className='login-page__user-info-input login-page__user-info-input--first'></input>
+                    <label for='password' className='login-page__user-info-label'>Password</label>
                     <input type='text' id='password' name='password' className='login-page__user-info-input'></input>
                 </form>
-                <div className='home-page__buttons'>
-                    <button type='submit' className='home-page__search-submit'>Pour Sauce</button>
+                <div className='login-page__buttons'>
+                    <button type='submit' className='login-page__search-submit'>Pour Sauce</button>
                     <button type='submit' className='home-page__login'>Log In</button>
                 </div>
             </div>
