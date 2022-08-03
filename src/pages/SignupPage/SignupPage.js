47da3c2ce4a104eb0e5d55b9d4dc14a686475e8d
Audun <audunyoung@gmail.com>
2022-08-02 19:50:14 -0600

    Set up signup page.

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -9,7 +9,16 @@ class SignupPage extends Component {
     render() {
         return (
             <div className='signup-page'>
-
+                    <form className='signup-page__user-info'>
+                    <label for='email' className='signup-page__user-info-label'>Email</label>
+                    <input type='text' id='email' name='email' className='signup-page__user-info-input signup-page__user-info-input--first'></input>
+                    <label for='password' className='signup-page__user-info-label'>Password</label>
+                    <input type='text' id='password' name='password' className='signup-page__user-info-input'></input>
+                </form>
+                <div className='signup-page__buttons'>
+                    <button type='submit' className='signup-page__search-submit'>Signup</button>
+                    <button type='submit' className='home-page__signup'>Cancel</button>
+                </div>
             </div>
         )
     }
