cf5b4ba026f74a5edeaf10c640f2f05e9b36c275
Audun <audunyoung@gmail.com>
2022-08-02 18:47:29 -0600

    Set up site routes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import PageHeader from './components/PageHeader/PageHeader';
 
 function App() {
   return (
     <Router>
+      <PageHeader />
       <Switch>
         <Route path='/' exact component={HomePage} />
+        {/* <Route path='/login' component={LoginPage} />*/}
+        {/* <Route path='/signup' component={SignupPage} />*/}
+        {/* <Route path='/login' component={LoginPage} />*/}
+        {/* <Route path='/restaurants' component={RestaurantList} />*/}
+        {/* <Route path='/restaurants/:restaurantId' component={Restaurant} />*/}
+        {/* <Route path='/restaurants/:restaurantId/:sauceId' component={SauceItem} />*/}
       </Switch>
     </Router>
   );
